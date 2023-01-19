e57c864d773bc4bb502901c2a312197720345254
Danielateresa <dany.t_87@hotmail.it>
2023-01-19 14:36:22 +0100

    Create API

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+   public function index()
+   {
+    //restituisce la lista dei progetti presenti nel database in formato json
+    return response()->json([
+        'success' => true,
+        'data' => Project::orderByDesc('id')->get()
+    ]);
+   }
+}
